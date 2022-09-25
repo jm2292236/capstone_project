436bb1087e4af22c6dfff3f4fbb5964621b4b4f0
Jaime Montes de Oca <jaime0422@live.com>
2022-09-24 23:13:08 -0700

    Added index & show properties actions

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,7 +1,19 @@
 class PropertiesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
     def index
         render json: Property.all
+    end
+
+    def show
+        property = Property.find(params[:id])
+        render json: property
+    end
+
+    private
+
+    def record_not_found
+      render json: { error: "Property not found" }, status: :not_found
     end
 
 end
